1ed781b96023146a1b3d1808a3239ed5b82a43e5
netsecexplained <netsecexplained@gmail.com>
2022-03-31 05:22:43 -0400

    updated db pass

--- file-inclusion/college_website/admin/db_connect.php
+++ file-inclusion/college_website/admin/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('mariadb','root','mysqlroot','college_website_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('mariadb','root','MysQlR00tP@ssw0rd','college_website_db')or die("Could not connect to mysql".mysqli_error($con));
